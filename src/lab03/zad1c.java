3f0f53f4bb792577592cbba53983ae0c0b26d2e6
Filip Wojnicki <filip.wojnicki@op.pl>
2021-11-06 12:11:47 +0000

    lab03 zad1 c

---
+++ src/lab03/zad1c.java
@@ -0,0 +1,22 @@
+package lab03;
+
+public class zad1c {
+    public static void main(String[] args) {
+        System.out.println(middle("middle"));
+    }
+
+    static String middle(String str) {
+        String result = "";
+
+        int strLength = str.length();
+
+        if(strLength % 2 == 0) {
+            result += Character.toString(str.charAt(strLength / 2 - 1));
+            result += Character.toString(str.charAt(strLength / 2));
+        } else {
+            result = Character.toString(str.charAt(strLength / 2));
+        }
+
+        return result;
+    }
+}
