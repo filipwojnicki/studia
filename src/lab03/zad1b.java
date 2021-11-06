ffe93afec2570c153b8de5a150373315f4a00afc
Filip Wojnicki <filip.wojnicki@op.pl>
2021-11-06 12:03:21 +0000

    lab03 zad1 b

---
+++ src/lab03/zad1b.java
@@ -0,0 +1,26 @@
+package lab03;
+
+public class zad1b {
+    public static void main(String[] args) {
+        System.out.println(countSubstring("Hello world world", "world"));
+    }
+
+    static int countSubstring(String str, String subStr) {
+        int count = 0;
+        int strLength = subStr.length();
+
+        int index = 0;
+
+        while(true) {
+            index = str.indexOf(subStr, index == 0 ? 0 : index + strLength);
+
+            if(index == -1) {
+                break;
+            }
+
+            count++;
+        }
+
+        return count;
+     }
+}
