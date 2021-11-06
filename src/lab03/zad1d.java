ca1d0c75f9c78282bc0bd26b921567878eb3b15d
Filip Wojnicki <filip.wojnicki@op.pl>
2021-11-06 12:15:04 +0000

    lab03 zad1 d

---
+++ src/lab03/zad1d.java
@@ -0,0 +1,17 @@
+package lab03;
+
+public class zad1d {
+    public static void main(String[] args) {
+        System.out.println(repeat("middle", 5));
+    }
+
+    static String repeat(String str, int n) {
+        String result = "";
+
+        for(int i = 0; i < n; i++) {
+            result += str;
+        }
+
+        return result;
+    }
+}
