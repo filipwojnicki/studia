8ff3638c98e6b8843e761a4732dadca25b9b5d23
Filip Wojnicki <filip.wojnicki@op.pl>
2021-11-21 13:33:44 +0100

    lab05 klasy zad56

--- src/lab03/klasy/Point.java
+++ src/lab03/klasy/Point.java
@@ -1,8 +1,8 @@
 package lab03.klasy;
 
 public final class Point {
-  private final double x;
-  private final double y;
+  private double x;
+  private double y;
 
   public Point(double x, double y) {
     this.x = x;
@@ -16,6 +16,17 @@ public final class Point {
   public Point scale(double s) {
     return new Point(x * s, y * s);
   }
+
+  public void translateModify(int dirX, int dirY) {
+    x += dirX;
+    y += dirY;
+  }
+
+  public void scaleModify(double s) {
+    x *= s;
+    y *= s;
+  }
+
 
   public double getX() { return x; }
   public double getY() { return y; }
--- src/lab03/klasy/zad56.java
+++ src/lab03/klasy/zad56.java
@@ -1,10 +1,12 @@
 package lab03.klasy;
 
-import lab03.klasy.Point;
-
-public class zad5 {
+public class zad56 {
   public static void main(String[] args) {
     Point p = new Point(3, 4).translate(1, 3).scale(0.5);
+    System.out.println("X = " + p.getX() + ", Y = " + p.getY());
+
+    p.translateModify(2,4);
+    p.scaleModify(0.5);
     System.out.println("X = " + p.getX() + ", Y = " + p.getY());
   }
 }
