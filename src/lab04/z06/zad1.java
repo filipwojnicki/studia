a347fd366d073643d4cd8190e444ac2b6cdb7241
Filip Wojnicki <filip.wojnicki@op.pl>
2021-12-12 13:31:04 +0100

    lab04 z06 zad1 EmployeeLocalDate

--- src/lab04/z06/zad1.java
+++ src/lab04/z06/zad1.java
@@ -9,11 +9,16 @@ public class zad1 {
     public static void main(String[] args)
     {
         Employee[] personel = new Employee[3];
+        EmployeeLocalDate[] personelLocalDate = new EmployeeLocalDate[3];
 
         // wypełnij tablicę danymi pracowników
         personel[0] = new Employee("Harry Cracker", 75000, 2001, 12, 15);
         personel[1] = new Employee("Caroline Hacker", 50000, 2003, 10, 1);
         personel[2] = new Employee("Antony Tester", 40000, 2005, 3, 15);
+
+        personelLocalDate[0] = new EmployeeLocalDate("Harry Cracker", 75000, LocalDate.of(2001, 12, 15));
+        personelLocalDate[1] = new EmployeeLocalDate("Caroline Hacker", 50000, LocalDate.of(2003, 10, 1));
+        personelLocalDate[2] = new EmployeeLocalDate("Antony Tester", 40000, LocalDate.of(2005, 3, 15));
 
         // zwieksz pobory każdego pracownika o 20%
         for (Employee e : personel) {
@@ -28,6 +33,13 @@ public class zad1 {
         }
         System.out.println();
 
+        for (EmployeeLocalDate e : personelLocalDate) {
+          System.out.print("nazwisko = " + e.getName() + "\tid = " + e.getId());
+          System.out.print("\tpobory = " + e.getSalary());
+          System.out.printf("\tdataZatrudnienia = %tF\n", e.getHireDay());
+      }
+      System.out.println();
+
         // Dodane w stosunku do poprzedniej wersji
         int n = Employee.getNextId(); // wywołanie metody statycznej
         System.out.println("Następny dostępny id = " + n);
@@ -35,8 +47,7 @@ public class zad1 {
     }
 }
 
-class Employee
-{
+class Employee {
     public Employee(String name, double salary, int year, int month, int day)
     {
         this.name = name;
@@ -99,3 +110,62 @@ class Employee
     private static int nextId = 1;
 }
 
+class EmployeeLocalDate {
+  public EmployeeLocalDate(String name, double salary, LocalDate hireDay) {
+      this.name = name;
+      this.salary = salary;
+      this.hireDay = java.sql.Date.valueOf(hireDay);
+
+      // Dodane w stosunku do poprzedniej wersji
+      id = nextId;
+      ++nextId;
+  }
+
+  public String getName()
+  {
+      return name;
+  }
+
+  public double getSalary()
+  {
+      return salary;
+  }
+
+  public Date getHireDay()
+  {
+      return (Date) hireDay.clone();
+  }
+
+  public void raiseSalary(double procent)
+  {
+      double raise = salary * procent / 100;
+      salary += raise;
+  }
+
+  // Dodane w stosunku do poprzedniej wersji
+  public int getId()
+  {
+      return id;
+  }
+
+  // Dodane w stosunku do poprzedniej wersji
+  public void setId()
+  {
+      id = nextId;
+      ++nextId;
+  }
+
+  // Dodane w stosunku do poprzedniej wersji
+  public static int getNextId()
+  {
+      return nextId;
+  }
+
+  private String name;
+  private double salary;
+  private Date hireDay;
+
+  // Dodane w stosunku do poprzedniej wersji
+  private int id;
+  private static int nextId = 1;
+}
