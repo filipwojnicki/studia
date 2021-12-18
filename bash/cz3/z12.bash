9760e9ec46a0d8a856fbb6e970127f0087cdcb82
Filip Wojnicki <filip.wojnicki@op.pl>
2021-12-19 00:41:42 +0100

    bash cz3 zad12

---
+++ bash/cz3/z12.bash
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+cat /dev/null > wynik
+
+filename='dane'
+resultFile='wynik'
+
+separator=' '
+
+if ! [[ -r $filename ]]; then
+  echo "Pliku nie udało sie odczytać"
+  exit 1
+fi
+
+
+while IFS=${separator} read line; do
+  numbers=($line)
+
+  if [[ ${numbers[0]} -gt ${numbers[1]} ]]; then 
+    echo ${numbers[0]}$separator${numbers[1]} >> resultFile
+  else
+    echo ${numbers[1]}$separator${numbers[0]} >> resultFile
+  fi
+done < $filename
