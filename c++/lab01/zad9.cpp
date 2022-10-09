9151d5d67144f2471db75e18c6118c55dc52aacd
Filip Wojnicki <filip.wojnicki@op.pl>
2022-10-09 22:45:30 +0200

    c++ lab01 zad9

---
+++ c++/lab01/zad9.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+// The maximum limit of int: 2147483647
+// The minimum limit of int: -2147483648
+// The maximum limit of unsigned int: 4294967295
+// The maximum limit of long long: 9223372036854775807
+// The minimum limit of long long: -9223372036854775808
+// The maximum limit of unsigned long long: 18446744073709551615
+// The Bits contain in char: 8
+// The maximum limit of char: 127
+// The minimum limit of char: -128
+// The maximum limit of signed char: 127
+// The minimum limit of signed char: -128
+// The maximum limit of unsigned char: 255
+// The minimum limit of short: -32768
+// The maximum limit of short: 32767
+// The maximum limit of unsigned short: 65535
+
+int main() {
+  cout << "INT_MAX" << " = " << INT_MAX << endl;
+  cout << "INT_MIN" << " = " << INT_MIN << endl;
+  cout << "UINT_MAX" << " = " << UINT_MAX << endl;
+  cout << "LLONG_MAX" << " = " << LLONG_MAX << endl;
+  cout << "LLONG_MIN" << " = " << LLONG_MIN << endl;
+  cout << "ULLONG_MAX" << " = " << ULLONG_MAX << endl;
+  cout << "CHAR_BIT" << " = " << CHAR_BIT << endl;
+  cout << "CHAR_MAX" << " = " << CHAR_MAX << endl;
+  cout << "CHAR_MIN" << " = " << CHAR_MIN << endl;
+  cout << "SCHAR_MAX" << " = " << SCHAR_MAX << endl;
+  cout << "SCHAR_MIN" << " = " << SCHAR_MIN << endl;
+  cout << "SHRT_MAX" << " = " << SHRT_MAX << endl;
+  cout << "SHRT_MIN" << " = " << SHRT_MIN << endl;
+  cout << "UCHAR_MAX" << " = " << UCHAR_MAX << endl;
+  cout << "USHRT_MAX" << " = " << USHRT_MAX  << endl;
+} 
