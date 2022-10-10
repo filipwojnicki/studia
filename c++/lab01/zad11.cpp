7cadaeeabf4c2a83b6502fbebcc9c292b652b549
Filip Wojnicki <filip.wojnicki@op.pl>
2022-10-10 02:06:11 +0200

    c++ lab01 zad11

--- c++/lab01/zad11.cpp
+++ c++/lab01/zad11.cpp
@@ -1,9 +1,42 @@
 #include <iostream>
+#include <limits.h>
 
 int main()
 {
     int a = 1000000, b = 1000000;
+
     long long int c = static_cast<long long int>(a)*b;
-    std::cout << c;
+    std::cout << a << " * " << a << " = " << c << std::endl;
+    
+    if (b > 0 && a > INT_MAX - b) {
+      std::cout << "Przepelnienie dodawanie";
+      return 1;
+    }
+
+    if (b < 0 && a < INT_MIN - b) {
+      std::cout << "Niedomiar dodawanie";
+      return 1;
+    }
+
+    if (b < 0 && a > INT_MAX + b) {
+      std::cout << "Przepelnienie odejmowanie";
+      return 1;
+    }
+
+    if (b > 0 && a < INT_MIN + b) {
+      std::cout << "Niedomiar odejmowanie";
+      return 1;
+    }
+
+    if (b != 0 && a > INT_MAX / b) {
+      std::cout << "Przepelnienie mnozenie";
+      return 1;
+    }
+
+    if (b != 0 && a < INT_MIN / b) {
+      std::cout << "Niedomiar mnozenie";
+      return 1;
+    }
+
     return 0;
 };
